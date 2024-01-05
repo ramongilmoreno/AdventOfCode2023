b381cd908eb75b2495d1193bbb901021adba3378
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2024-01-05 03:04:13 +0100

    Day 16 part 1 no issues, only Python difficulties

---
+++ 16-1/parse.py
@@ -0,0 +1,101 @@
+import sys
+
+def log (x, end = "\n"):
+  # print(x, end = end)
+  sys.stdout.flush()
+  pass
+
+print("Begin")
+
+rows = []
+for line in sys.stdin:
+  line = line.strip()
+  log(f"Line [{line}]")
+  if len(line) > 0:
+    rows.append(line)
+
+height = len(rows)
+width = len(rows[0])
+
+def get (x, y): return rows[y][x]
+
+def print_field (visited): 
+  for i in range(height):
+    for j in range(width):
+      if str((j, i)) in visited:
+        log('#', end="")
+      else:
+        log(get(j, i), end="")
+    log("")
+
+class Direction:
+  def __init__(self, deltaX, deltaY):
+    self.deltaX = deltaX
+    self.deltaY = deltaY
+  # def perpendicular (self): return [Direction(-self.deltaY, self.deltaX), Direction(self.deltaY, -self.deltaX)]
+  def left (self): return Direction(self.deltaY, -self.deltaX)
+  def right (self): return Direction(-self.deltaY, self.deltaX)
+  def perpendicular (self): return [self.left(), self.right()]
+  def respond2 (self, x):
+    if x == '.':
+      return [self]
+    if x == '|':
+      return [self] if self.deltaY != 0 else self.perpendicular()
+    elif x == '-':
+      return [self] if self.deltaX != 0 else self.perpendicular()
+    elif x == '/':
+      if self.deltaX != 0:
+        return [self.left()]
+      else:
+        return [self.right()]
+    elif x == '\\':
+      if self.deltaX != 0:
+        return [self.right()]
+      else:
+        return [self.left()]
+  def respond (self, x):
+    r = self.respond2(x)
+    log(f"For direction {self}, item {x}, respond with {r}")
+    return r
+
+  def __repr__(self): return str((self.deltaX, self.deltaY))
+
+NORTH = Direction(0, -1)
+SOUTH = Direction(0, 1)
+WEST = Direction(-1, 0)
+EAST = Direction(1, 0)
+directions = [ NORTH, SOUTH, WEST, EAST ]
+
+for direction in directions:
+  log(f"Direction {direction} perpendicular {direction.perpendicular()} left {direction.left()} right {direction.right()}")
+
+beams = [ (0, 0, EAST) ]
+visited = {}
+visited_and_direction = {}
+flat_map = lambda f, xs: [y for ys in xs for y in f(ys)]
+
+def move (beam):
+  x = beam[0]
+  y = beam[1]
+  d = beam[2]
+
+  if str((x, y, d)) in visited_and_direction:
+    return []
+
+  if x < 0 or x >= width or y < 0 or y >= height:
+    return []
+
+  visited[str((x, y))] = (x, y)
+  visited_and_direction[str((x, y, d))] = True
+
+  item = get(x, y)
+  return list(map(lambda i: (x + i.deltaX, y + i.deltaY, i), d.respond(item)))
+
+while len(beams) > 0:
+  beams = flat_map(move, beams)
+  log(f"Beams after {beams}")
+  print_field(visited)
+  
+
+print(f'Answer: {len(visited.keys())}')
+
---
+++ 16-1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+f () { echo $1; cat $1 | python3 parse.py | sed "s/^/    /g"; }
+f example.txt
+f input.txt
