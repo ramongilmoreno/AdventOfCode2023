0104663759ad3af344afed2f71426f6639f4d768
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2024-01-12 01:54:39 +0100

    Lazy calculation of all possible intervals and one-by-one processing of rules; reduced memory. Example OK, but unsure yet about input.txt

--- 19-2/parse.py
+++ 19-2/parse.py
@@ -44,27 +44,43 @@ for line in sys.stdin:
             return target
           else:
             return None
-        return { "function": r, "condition_field": field, "condition_param": value, "condition_operation": f.__name__}
+        return { "function": r, "target": target, "condition_field": field, "condition_param": value, "condition_operation": f.__name__}
       else:
         log(f"Rule direct -> {step}")
-        return { "function": lambda x: step, "condition_field": None }
+        return { "function": lambda x: step, "target": step, "condition_field": None }
     workflows[name] = list(map(parse_steps, steps))
     log(f"Workflows {workflows}")
   else:
     break
 
+log(f"Workflow {workflows}")
+
+def steps_for_target (target):
+  r = []
+  for workflow_name in workflows:
+    workflow = workflows[workflow_name]
+    if next((condition for condition in workflow if condition["target"] == target), None) != None:
+      for condition in workflow:
+        r.append(condition)
+      r.extend(steps_for_target(workflow_name))
+  return r
+
+smaller_set_of_steps = steps_for_target('A')
+log(f"Steps for target {smaller_set_of_steps}")
+
 # Compute candidate intervals based on workflows conditions for each field
+intervals_count = 1
 def intervals (key):
+  global intervals_count
   greater_than = set()
   smaller_than = set()
-  for workflow in workflows.values():
-    for condition in workflow:
-      if condition["condition_field"] == key:
-        op = condition["condition_operation"]
-        if op == "greater_than":
-          greater_than.add(condition["condition_param"])
-        else:
-          smaller_than.add(condition["condition_param"])
+  for condition in smaller_set_of_steps:
+    if condition["condition_field"] == key:
+      op = condition["condition_operation"]
+      if op == "greater_than":
+        greater_than.add(condition["condition_param"])
+      else:
+        smaller_than.add(condition["condition_param"])
   greater_than.add(4000)
   r = []
   all = list(greater_than.union(smaller_than))
@@ -81,68 +97,59 @@ def intervals (key):
      elif v in greater_than:
        r.append((current[0], v))
        current = (v + 1, None)
-  log(f"For {key} intervals {r}, count {len(r)}")
+  print(f"For {key} intervals count {len(r)}")
+  sys.stdout.flush()
+  intervals_count *= len(r)
   return r
 intervals_x = intervals('x')
 intervals_m = intervals('m')
 intervals_a = intervals('a')
 intervals_s = intervals('s')
 
-parts = []
-index = 0
-for i_x in intervals_x:
-  for i_m in intervals_m:
-    for i_a in intervals_a:
-      for i_s in intervals_s:
-        parts.append({ "x": i_x, "m": i_m, "a": i_a, "s": i_s, "order": index })
-        index += 1
-
-log(f"Parts {parts}")
-print(f"Number of tests {len(parts)}")
-
-# Prepare agenda with the first value of each interval
-agenda = list(map(lambda x: ('in', x), map(lambda x: { "x": x["x"][0], "m": x["m"][0], "a": x["a"][0], "s": x["s"][0], "order": x["order"]}, parts)))
-log(f"Agenda {agenda}")
-
-R = []
-A = []
-while len(agenda) > 0:
-  log(f'Agenda {agenda}')
-  # Process each item
-  r = []
-  for i in agenda:
-    part = i[1]
-    workflow = workflows[i[0]]
-    skip = False
-    for j in range(len(workflow)):
-      log(f'Processing rule {j}') 
-      if not skip:
-        s = workflow[j]
-        r2 = s["function"](part)
-        if r2 != None:
-          log(f"Applied workflow {i[0]}, rule {j} to part {part} -> {r2}")
-        if r2 == 'A':
-          A.append(part)
-          skip = True
-        elif r2 == 'R':
-          R.append(part)
-          skip = True
-        elif r2 != None:
-          r.append((r2, part))
-          skip = True
-  agenda = r
-
-
-log(f'A: {A}')
-log(f'R: {R}')
-
 def size_of_interval (x):
   return x[1] - x[0] + 1
+def all_options ():
+  for i_x in intervals_x:
+    for i_m in intervals_m:
+      for i_a in intervals_a:
+        for i_s in intervals_s:
+          yield { "x": i_x, "m": i_m, "a": i_a, "s": i_s, "size": size_of_interval(i_x) * size_of_interval(i_m) * size_of_interval(i_a) * size_of_interval(i_s) }
+
 acc = 0
-for i in A:
-  part = parts[i["order"]]
-  log(f"Sample {i} of part {part} accepted")
-  acc += size_of_interval(part["x"]) * size_of_interval(part["m"]) * size_of_interval(part["a"]) * size_of_interval(part["s"])
+count = 0
+for o in all_options():
+
+  # Prepare agenda with the first value of each interval
+  agenda = [('in', { "x": o["x"][0], "m": o["m"][0], "a": o["a"][0], "s": o["s"][0], "size": o["size"]})]
+  log(f"Agenda {agenda}")
+
+  while len(agenda) > 0:
+    log(f'Agenda {agenda}')
+    # Process each item
+    r = []
+    for i in agenda:
+      part = i[1]
+      workflow = workflows[i[0]]
+      skip = False
+      for j in range(len(workflow)):
+        log(f'Processing rule {j}') 
+        if not skip:
+          s = workflow[j]
+          r2 = s["function"](part)
+          if r2 != None:
+            log(f"Applied workflow {i[0]}, rule {j} to part {part} -> {r2}")
+          if r2 == 'A':
+            acc += part["size"]
+            skip = True
+          elif r2 == 'R':
+            skip = True
+          elif r2 != None:
+            r.append((r2, part))
+            skip = True
+    agenda = r
+  count += 1
+  if count % 100000 == 0:
+    print(f"Acc at iteration {count} of {intervals_count} ({(count / intervals_count) * 100} %) is {acc}")
 
 print(f'Answer: {acc}')
 
--- 19-2/run.sh
+++ 19-2/run.sh
@@ -2,6 +2,4 @@
 
 f () { echo $1; cat ../19-1/$1 | python3 parse.py | sed "s/^/    /g"; }
 f example.txt
-echo "Not fast enough for input.txt"
-exit -1
 f input.txt
